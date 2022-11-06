80e08694efe5c4aac31b020fd19dad1eca0496fe
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-11-06 12:46:46 +0200

    added day of week solution

---
+++ src/01. Syntax, Functions and Statements/dayOfWeek.ts
@@ -0,0 +1,40 @@
+type DayOfWeek =
+  | 'Monday'
+  | 'Tuesday'
+  | 'Wednesday'
+  | 'Thursday'
+  | 'Friday'
+  | 'Saturday'
+  | 'Sunday'
+
+function dayOfWeek(day: DayOfWeek) {
+  switch (day) {
+    case 'Monday':
+      console.log(1)
+      break
+    case 'Tuesday':
+      console.log(2)
+      break
+    case 'Wednesday':
+      console.log(3)
+      break
+    case 'Thursday':
+      console.log(4)
+      break
+    case 'Friday':
+      console.log(5)
+      break
+    case 'Saturday':
+      console.log(6)
+      break
+    case 'Sunday':
+      console.log(7)
+      break
+    default:
+      console.log('error')
+  }
+}
+
+dayOfWeek('Friday')
+
+export {}
