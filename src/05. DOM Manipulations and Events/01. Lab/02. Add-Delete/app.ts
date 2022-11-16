06de37dfaba68f4867661c62dd22cc0ee7507f41
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-11-16 23:23:17 +0200

    added function that read the text inside an input field and appends the specified text to a list inside an html page and clicking it, delete the item with no confirmation

---
+++ src/05. DOM Manipulations and Events/01. Lab/02. Add-Delete/app.ts
@@ -0,0 +1,38 @@
+type Nullable<T> = T | null
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function addItem() {
+  const input = document.getElementById(
+    'newItemText',
+  ) as Nullable<HTMLInputElement>
+
+  if (!input) {
+    return
+  }
+
+  const liElement = createElementWithContent('li', input.value)
+  const items = document.getElementById('items') as Nullable<HTMLUListElement>
+  const deleteBtn = createElementWithContent('a', '[Delete]')
+  deleteBtn.href = '#'
+
+  deleteBtn.addEventListener('click', (e: MouseEvent) => {
+    const target = e.target as HTMLAnchorElement
+    target.parentNode?.removeChild(target)
+  })
+
+  liElement.appendChild(deleteBtn)
+  items?.appendChild(liElement)
+  input.value = ''
+
+  function createElementWithContent<K extends keyof HTMLElementTagNameMap>(
+    type: K,
+    content: string,
+  ) {
+    const element = document.createElement(type)
+    element.textContent = content
+
+    return element
+  }
+}
+
+export {}
