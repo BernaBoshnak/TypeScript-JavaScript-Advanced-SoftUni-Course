36a839555c741fa189aaad46286bae5efccd84d4
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-11-17 12:05:09 +0200

    added generic type

--- src/05. DOM Manipulations and Events/01. Lab/01. List-Of-Items/list.ts
+++ src/05. DOM Manipulations and Events/01. Lab/01. List-Of-Items/list.ts
@@ -1,13 +1,21 @@
+type Nullable<T> = T | null
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function addItem() {
-  const items = document.getElementById('items') as HTMLElement
-  const text = document.getElementById('newItemText') as HTMLInputElement
+  const items = document.getElementById('items') as Nullable<HTMLElement>
+  const input = document.getElementById(
+    'newItemText',
+  ) as Nullable<HTMLInputElement>
+
+  if (!input) {
+    return
+  }
 
   const liElement = document.createElement('li')
-  liElement.textContent = text.value
+  liElement.textContent = input.value
 
-  items.appendChild(liElement)
-  text.value = ''
+  items?.appendChild(liElement)
+  input.value = ''
 }
 
 export {}
