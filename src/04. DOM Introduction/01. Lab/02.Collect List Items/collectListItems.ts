aafd07431eb598a8caf6d300855018579f761a95
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-11-17 16:12:32 +0200

    added generic type

--- src/04. DOM Introduction/01. Lab/02.Collect List Items/collectListItems.ts
+++ src/04. DOM Introduction/01. Lab/02.Collect List Items/collectListItems.ts
@@ -1,14 +1,18 @@
-const btn = document.getElementById('btn') as HTMLButtonElement
+type Nullable<T> = T | null
 
-if (btn) {
-  btn.addEventListener('click', () => {
-    const liElements = [
-      ...document.getElementsByTagName('li'),
-    ] as HTMLLIElement[]
-    const elementText = liElements.map((el) => el.textContent)
-    const textArea = document.getElementById('result') as HTMLTextAreaElement
-    textArea.value = elementText.join('\n')
-  })
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function extractText() {
+  const liElements = Array.from(document.querySelectorAll<HTMLLIElement>('li'))
+  const elementText = liElements.map((el) => el.textContent)
+  const textArea = document.getElementById(
+    'result',
+  ) as Nullable<HTMLTextAreaElement>
+
+  if (!textArea) {
+    return
+  }
+
+  textArea.value = elementText.join('\n')
 }
 
 export {}
