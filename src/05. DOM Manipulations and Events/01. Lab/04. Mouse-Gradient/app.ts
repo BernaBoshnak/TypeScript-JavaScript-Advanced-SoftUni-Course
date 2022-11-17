caf89c26d020105a3914b6bc215d2d5a1beaccf4
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-11-17 12:21:08 +0200

    added generic type

--- src/05. DOM Manipulations and Events/01. Lab/04. Mouse-Gradient/app.ts
+++ src/05. DOM Manipulations and Events/01. Lab/04. Mouse-Gradient/app.ts
@@ -1,15 +1,19 @@
+type Nullable<T> = T | null
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function attachGradientEvents() {
-  const divEl = document.getElementById('gradient') as HTMLDivElement
-  const output = document.getElementById('result') as HTMLDivElement
-  divEl.addEventListener('mousemove', onClick)
+  const divEl = document.getElementById('gradient') as Nullable<HTMLDivElement>
+  const output = document.getElementById('result') as Nullable<HTMLDivElement>
+  divEl?.addEventListener('mousemove', onClick)
 
   function onClick(e: MouseEvent) {
     const offsetX = e.offsetX
     const target = e.target as HTMLElement
     const percent = Math.trunc((offsetX / target.clientWidth) * 100)
 
-    output.textContent = `${percent}%`
+    if (output) {
+      output.textContent = `${percent}%`
+    }
   }
 }
 
